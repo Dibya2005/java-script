d9f2d633105729f30ff4d2bb8ba6c5f87f33adee
Dibyajyoti Ray Choudhury <141916458+Dibya2005@users.noreply.github.com>
2025-05-30 02:14:23 +0530

    Add files via upload

---
+++ eventjs/evilisten2.js
@@ -0,0 +1,40 @@
+
+
+// function changeText(event) {
+//     console.log(event);
+//     let fpara = document.getElementById('fpara');
+//     fpara.textContent = " Babbar"
+// }
+
+// let fpara = document.getElementById('fpara');
+
+// fpara.addEventListener('click', changeText); 
+
+//fpara.removeEventListener('click', changeText);
+
+// let anchorElement = document.getElementById('fanchor');
+
+// anchorElement.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     anchorElement.textContent = "Click done bhai"
+// });
+// console.log("Hi");
+//  let paras = document.querySelectorAll('p');
+
+function alertPara(event) {
+  //span t click krli slbo para t click  krli nosla
+    if(event.target.nodeName === 'SPAN') {
+        alert("You have Clicked on para: " + event.target.textContent );
+    }
+    
+}
+
+// for(let i=0; i<paras.length; i++) {
+//     let para = paras[i];
+//     console.log("eventListener added");
+//     para.addEventListener('click', alertPara);
+// }
+
+let mydiv = document.getElementById('wrapper');
+
+document.addEventListener('click', alertPara);
---
+++ eventjs/evlisten.js
@@ -0,0 +1,8 @@
+function changetext(){
+  let fpera=document.getElementById("fpera")
+  fpera.textContent="joisk"
+}
+let fpera=document.getElementById("fpera")
+// fpera.addEventListener('click',changetext())
+//click nkrata function call hb
+fpera.addEventListener('click',changetext)
